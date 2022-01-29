d40a352b9c01a2568d14b5326a65acc2a0e1b5a8
mostofa kamal <mostofakamal1990@gmail.com>
2022-01-29 22:52:16 +0600

    is odd or even number

---
+++ even-odd.js
@@ -0,0 +1,28 @@
+function isEven(evenorodd){
+    if(evenorodd %2 ==0){
+        return true;
+    }
+
+    return false;
+}
+
+
+const isNumberEven = isEven(334);
+
+const isHernumbrEven = isEven(3335);
+
+
+// cheack odd numver
+
+function isOdd(odd){
+    if(odd % 2 != 0){
+        return true;
+    }
+
+    return false;
+}
+
+const isOddNumber = isOdd(555);
+
+
+const herNumberIsOdd = isOdd(6668);
