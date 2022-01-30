285f312bae801e873736436668a8e8c0871ff149
mostofa kamal <mostofakamal1990@gmail.com>
2022-01-30 09:22:30 +0600

    Factorial

---
+++ factorial.js
@@ -0,0 +1,28 @@
+let factorial = 1;
+
+
+for(var i = 1; i<= 7; i++){
+    
+    factorial = factorial * i;
+}
+
+
+//console.log(factorial);
+
+
+function factorials(fact){
+
+
+    let factorial = 1;
+
+
+    for(let i = 1; i<= fact; i++){
+        
+        factorial = factorial * i;
+    }
+
+    return factorial;
+    
+}
+
+console.log(factorials(5));
