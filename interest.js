44ecf20b39ce1a2523bc50ee77e7b161b2496932
mostofa kamal <mostofakamal1990@gmail.com>
2022-01-30 23:13:10 +0600

    interest calculators

---
+++ interest.js
@@ -0,0 +1,19 @@
+function getInterst(princeple, total, time){
+
+    if( princeple <= 0  || total <= 0){
+        return " place give valid princeple or inrest";
+    }
+    let interstAmount = total - princeple;
+
+    // interst formula 
+
+    let interstCalculate = (interstAmount * 100 )/ (princeple * time);
+
+    return interstCalculate + " % ";
+
+}
+
+
+console.log(getInterst(500, 600, 1));
+console.log(getInterst(5000, 6000, 2));
+console.log(getInterst(4000, 6000, 1));
